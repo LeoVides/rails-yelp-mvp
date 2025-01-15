ac11346ceec5702523699e47602718275561c27c
Leonardo Vides <leovides9@gmail.com>
2025-01-15 13:19:19 +0100

    Saving progress

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,20 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+
+require "faker"
+
+# 1. clean database
+puts "Cleaning database"
+Restaurant.destroy_all
+# 2. create the instances
+5.times do {
+  Restaurant.create!(
+    name: Faker::Restaurant.name,
+    address: Faker::Address.full_address,
+    phone_number: Faker::PhoneNumber.phone_number_with_country_code,
+    category: %w(chinese italian japanese french belgian).sample
+  )
+}
+# 3. display message
+puts "Finished! Created #{Restaurant.count} restaurants"
